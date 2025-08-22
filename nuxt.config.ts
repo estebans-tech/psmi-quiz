9e31356595bd7eecbd69a077e71e5b65186ab9ea
estebans-tech <estebanstech@gmail.com>
2025-08-22 20:57:05 +0200

    fix: nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/schema'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
