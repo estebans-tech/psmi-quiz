489f1f3821881abbbd30d6fbf237e46a945fb0ce
estebans-tech <estebanstech@gmail.com>
2025-08-26 00:33:38 +0200

    feat: add netlifx configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // SSR är default. Låt Nitro auto-detektera Netlify.
+  ssr: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
